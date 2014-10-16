43ba6a95ec40362599e1f47d86dbbe61899f32c0
lawshe <jelawshe@gmail.com>
2014-10-15 20:53:21 -0500

    single quotes and camel case

--- client/templates/main.js
+++ client/templates/main.js
@@ -2,27 +2,27 @@ Session.setDefault('section', '');
 if (Meteor.isClient) {
 	//---RENDERED
 	Template.fullpage.rendered = function() {
-		$(".container").show();
-	}
+		$('.container').show();
+	};
 	Template.home.rendered = function() {
 		Meteor.myFunctions.pageReady();
-		$("#fp-nav").hide();
-	}
+		$('#fp-nav').hide();
+	};
 	Template.about.rendered = function() {
 		Meteor.myFunctions.pageReady();
-	}
+	};
 	Template.work.rendered = function() {
 		Meteor.myFunctions.pageReady();
-	}
+	};
 	Template.landes.rendered = function() {
 		Meteor.myFunctions.pageReady();
-	}
+	};
 	Template.bpb.rendered = function() {
 		Meteor.myFunctions.pageReady();
-	}
+	};
 	Template.notfound.rendered = function() {
 		Meteor.myFunctions.pageReady();
-	}
+	};
 
 	//--EVENTS
 	Template.menu.events = {
@@ -33,45 +33,45 @@ if (Meteor.isClient) {
 		    Meteor.myFunctions.toggleMobileMenu();
 		},
 		'click .menu-item.active': function(){
-			var menu_id = $('.menu-item.active').attr("id");
-			if(menu_id == "menu-item-work"){
+			var menuId = $('.menu-item.active').attr('id');
+			if(menuId === 'menu-item-work'){
 			    Meteor.myFunctions.leavingPage();
-				window.location ="/work";
+				window.location ='/work';
 			}else{
 				location.reload();
 			}
-		},
-	}
+		}
+	};
 	Template.fullpage.events = {
 		'click .fullpage_covered': function(){
 			Meteor.myFunctions.checkMenu();
 		}
-	}
+	};
 	Template.home.events = {
 		'click .internal_link': function(){
 		    Meteor.myFunctions.leavingPage();
 		}
-	}
+	};
 	Template.about.events = {
 		'click .internal_link': function(){
 		    Meteor.myFunctions.leavingPage();
 		}
-	}
+	};
 	Template.work.events = {
 		'click .link_internal': function(){
 		    Meteor.myFunctions.leavingPage();
 		}
-	}
+	};
 	Template.landes.events = {
 		'click .link_internal': function(){
 		    Meteor.myFunctions.leavingPage();
 		}
-	}
+	};
 	Template.bpb.events = {
 		'click .link_internal': function(){
 		    Meteor.myFunctions.leavingPage();
 		}
-	}
+	};
 }
 
 if (Meteor.isServer) {
