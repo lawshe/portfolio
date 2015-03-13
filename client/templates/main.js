994572c94052ce55852606de5aba75352ff720c2
lawshe <jessica@jlawshe.com>
2015-03-13 11:43:51 -0500

    template hooks and events

--- client/templates/main.js
+++ client/templates/main.js
@@ -1,41 +1,106 @@
 Session.setDefault('section', '');
+Session.setDefault('page','');
+Session.setDefault('imgSize','sm');
 if (Meteor.isClient) {
-	//---RENDERED
-	Template.fullpage.rendered = function() {
-		$('.container').show();
-	};
-	Template.home.rendered = function() {
-		Meteor.myFunctions.pageReady();
-		$('#fp-nav').hide();
-	};
-	Template.about.rendered = function() {
-		Meteor.myFunctions.pageReady();
-	};
-	Template.work.rendered = function() {
-		Meteor.myFunctions.pageReady();
-	};
-	Template.landes.rendered = function() {
-		Meteor.myFunctions.pageReady();
-	};
-	Template.bpb.rendered = function() {
-		Meteor.myFunctions.pageReady();
-	};
-	Template.notfound.rendered = function() {
-		Meteor.myFunctions.pageReady();
-	};
+	Template.layout.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			$('.container').show();
+		},
+		destroyed: function () {
+		}
+	});
+	Template.home.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			Meteor.fxns.allPages();
+
+			Session.set('page','home');
+		},
+		destroyed: function () {
+			Meteor.fullpage.destroy();
+		}
+	});
+	Template.about.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			Meteor.fxns.allPages();
+			Meteor.about.me();
+			Session.set('page','about');
+		},
+		destroyed: function () {
+			Meteor.fullpage.destroy();
+		}
+	});
+	Template.work.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			Meteor.fxns.allPages();
+			Session.set('page','work');
+		},
+		destroyed: function () {
+			Meteor.fullpage.destroy();
+		}
+	});
+	//landes
+	Template.landes.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			Meteor.fxns.allPages();
+			Session.set('page','abolandesut');
+		},
+		destroyed: function () {
+			Meteor.fullpage.destroy();
+		}
+	});
+	//bpp
+	Template.bpb.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			Meteor.fxns.allPages();
+			Session.set('page','bpb');
+		},
+		destroyed: function () {
+			Meteor.fullpage.destroy();
+		}
+	});
+	//bim
+	Template.bim.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			Meteor.fxns.allPages();
+			Session.set('page','bim');
+		},
+		destroyed: function () {
+			Meteor.fullpage.destroy();
+		}
+	});
+	Template.notFound.hooks({
+		created: function () {
+		},
+		rendered: function () {
+			Meteor.fxns.allPages();
+		},
+		destroyed: function () {
+			Meteor.fullpage.destroy();
+		}
+	});
 
 	//--EVENTS
 	Template.menu.events = {
-		'click .menu-item': function(){
-		    Meteor.myFunctions.leavingPage();
-		},
 		'click .mobile_menu_toggle': function(){
-		    Meteor.myFunctions.toggleMobileMenu();
+		    Meteor.fxns.toggleMobileMenu();
 		},
 		'click .menu-item.active': function(){
 			var menuId = $('.menu-item.active').attr('id');
 			if(menuId === 'menu-item-work'){
-			    Meteor.myFunctions.leavingPage();
 				window.location ='/work';
 			}else{
 				location.reload();
@@ -44,37 +109,59 @@ if (Meteor.isClient) {
 	};
 	Template.fullpage.events = {
 		'click .fullpage_covered': function(){
-			Meteor.myFunctions.checkMenu();
+			Meteor.fxns.checkMenu();
 		}
 	};
 	Template.home.events = {
-		'click .internal_link': function(){
-		    Meteor.myFunctions.leavingPage();
+		'click .scroll-btn': function(e){
+			e.preventDefault();
+			Meteor.fullpage.nextSection();
 		}
 	};
 	Template.about.events = {
-		'click .internal_link': function(){
-		    Meteor.myFunctions.leavingPage();
+		'click .scroll-btn': function(e){
+			e.preventDefault();
+			Meteor.fullpage.nextSection();
+		},
+		'click .toSlide': function(e,t){
+			e.preventDefault();
+			Meteor.fxns.moveToSlide(e);
 		}
 	};
 	Template.work.events = {
-		'click .link_internal': function(){
-		    Meteor.myFunctions.leavingPage();
+		'click .scroll-btn': function(e){
+			e.preventDefault();
+			Meteor.fullpage.nextSection();
 		}
 	};
 	Template.landes.events = {
-		'click .link_internal': function(){
-		    Meteor.myFunctions.leavingPage();
+		'click .scroll-btn': function(e){
+			e.preventDefault();
+			Meteor.fullpage.nextSection();
+		},
+		'click .toSlide': function(e,t){
+			e.preventDefault();
+			Meteor.fxns.moveToSlide(e);
 		}
 	};
 	Template.bpb.events = {
-		'click .link_internal': function(){
-		    Meteor.myFunctions.leavingPage();
+		'click .scroll-btn': function(e){
+			e.preventDefault();
+			Meteor.fullpage.nextSection();
+		},
+		'click .toSlide': function(e,t){
+			e.preventDefault();
+			Meteor.fxns.moveToSlide(e);
 		}
 	};
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-  });
+	Template.bim.events = {
+		'click .scroll-btn': function(e){
+			e.preventDefault();
+			Meteor.fullpage.nextSection();
+		},
+		'click .toSlide': function(e,t){
+			e.preventDefault();
+			Meteor.fullpage.moveToSlide(e);
+		}
+	};
 }
