6e6cdba7d06d3a58d25a9870c10ca0050bd05c05
lawshe <jessica@jlawshe.com>
2015-05-04 17:50:29 -0500

    added sitemap package

---
+++ server/sitemaps.js
@@ -0,0 +1,9 @@
+sitemaps.add('/sitemap.xml', function() {
+  return [
+    { page: '/', lastmod: '2015-05-04T12:00:00+00:00' },
+    { page: '/work', lastmod: '2015-05-04T12:00:00+00:00' },
+    { page: '/work/landes', lastmod: '2015-05-04T12:00:00+00:00' },
+    { page: '/work/bpb', lastmod: '2015-05-04T12:00:00+00:00' },
+    { page: '/work/bim_cubed', lastmod: '2015-05-04T12:00:00+00:00' },
+  ];
+});
