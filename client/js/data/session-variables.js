22dbd2ad3af5dce91ff13e6e423a7afb74244c6b
lawshe <jessica@jlawshe.com>
2017-03-11 12:27:15 -0600

    Session variable defaults

---
+++ client/js/data/session-variables.js
@@ -0,0 +1,6 @@
+Session.setDefault('section', '');
+Session.setDefault('page','');
+Session.setDefault('imgSize','sm');
+Session.setDefault('ie',false);
+Session.setDefault('pageDescription','');
+Session.setDefault('pageKeywords','');
