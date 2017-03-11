e5505914d0aab8d0e48624444b07af1d92b63b6d
lawshe <jessica@jlawshe.com>
2017-03-11 12:03:31 -0600

    Skills list

---
+++ web-skills.js
@@ -0,0 +1,131 @@
+webSkills = [
+  {
+    what: 'Altmetrics API',
+    url: 'http://api.altmetric.com/'
+  },
+  {
+    what: 'Amazon Web Services (AWS)',
+    url: 'http://aws.amazon.com/'
+  },
+  {
+    what: 'AWS EC2',
+    url: 'https://aws.amazon.com/ec2/'
+  },
+  {
+    what: 'AWS Lambda',
+    url: 'https://aws.amazon.com/lambda/'
+  },
+  {
+    what: 'AWS S3',
+    url: 'https://aws.amazon.com/s3/'
+  },
+  {
+    what: 'Authorize.net API',
+    url: 'http://www.authorize.net/'
+  },
+  {
+    what: 'Bitbucket',
+    url: 'https://bitbucket.org/'
+  },
+  {
+    what: 'Bootstrap',
+    url: 'http://getbootstrap.com/',
+    what: 'Bootstrap'
+  },
+  {
+    what: 'CrossRef API',
+    url: 'http://search.crossref.org/help/api'
+  },
+  {
+    what: 'Dropbox API',
+    url: 'https://www.dropbox.com/developers'
+  },
+  {
+    what: 'Express',
+    url: 'http://expressjs.com/'
+  },
+  {
+    what: 'Facebook API',
+    url: 'https://developers.facebook.com/'
+  },
+  {
+    what: 'Flux',
+    url: 'https://facebook.github.io/flux/docs/in-depth-overview.html#content'
+  },
+  {
+    what: 'GitHub',
+    url: 'https://github.com/'
+  },
+  {
+    what: 'GitLab',
+    url: 'https://about.gitlab.com/'
+  },
+  {
+    what: 'Google Analytics',
+    url: 'http://www.google.com/analytics/'
+  },
+  {
+    what: 'Google Chrome Extensions',
+    url: 'https://developer.chrome.com/extensions/api_index'
+  },
+  {
+    what: 'Grafico.js',
+    url: 'http://grafico.kilianvalkhof.com/'
+  },
+  {
+    what: 'Foundation',
+    url: 'http://foundation.zurb.com/'
+  },
+  {
+    what: 'jQuery',
+    url: 'http://grafico.kilianvalkhof.com/'
+  },
+  {
+    what: 'MailChimp',
+    url: 'http://mailchimp.com/'
+  },
+  {
+    what: 'Materialize',
+    url: 'http://materializecss.com/'
+  },
+  {
+    what: 'Meteor',
+    url: 'https://www.meteor.com/'
+  },
+  {
+    what: 'Modernizr',
+    url: 'http://modernizr.com/'
+  },
+  {
+    what: 'Node.js',
+    url: 'https://nodejs.org/'
+  },
+  {
+    what: 'PubMed Central API',
+    url: 'http://www.ncbi.nlm.nih.gov/pmc/'
+  },
+  {
+    what: 'React.js',
+    url: 'https://facebook.github.io/react/'
+  },
+  {
+    what: 'RethinkDB',
+    url: 'https://www.rethinkdb.com/'
+  },
+  {
+    what: 'Shopify',
+    url: 'https://docs.shopify.com/api'
+  },
+  {
+    what: 'three.js',
+    url: 'http://threejs.org/'
+  },
+  {
+    what: 'Twitter API',
+    url: 'https://dev.twitter.com/'
+  },
+  {
+    what: 'WordPress',
+    url: 'https://wordpress.com/'
+  }
+];
