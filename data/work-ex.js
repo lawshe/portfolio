06c9a30ff29067e3dfbea464e28ed7ea89427e0d
lawshe <jessica@jlawshe.com>
2017-03-11 13:32:05 -0600

    List text - elaborated

--- data/work-ex.js
+++ data/work-ex.js
@@ -18,16 +18,6 @@ workExs = [
     desc: 'A secure checkout with credit card processing. Payments are processed via <a href="https://www.authorize.net/" target="_BLANK">Authorize.net\'s</a> API. Upon completing, the user receives a confirmation email &amp; access to articles purchased.'
   },
   {
-    title: 'Animated Logo',
-    client: 'bim_cubed',
-    image: {
-      imageClasses : 'no-bg',
-      file: 'home_iphone.gif',
-      title: 'Interactive Logo'
-    },
-    desc: 'Transformed client\'s logo into an interactive object.'
-  },
-  {
     title: 'Article Tweeting',
     client: 'landes',
     desc: 'For a peer reviewed bioscience research publisher, the editors skip logging into individual Twitter accounts &amp; tweet via the admin page. The same tweet can be tweeted by multiple Twitter accounts. Other journals can also be tagged in the tweet. After tweeting, a retweet option is displayed.',
@@ -43,6 +33,6 @@ workExs = [
       file: 'guide.png',
       title: 'City Guide'
     },
-    desc: 'Built a wedding RSVP manager for my wedding using <a href="https://facebook.github.io/react/" target="_BLANK">React</a>, <a href="https://facebook.github.io/flux/docs/in-depth-overview.html#content" target="_BLANK">Flux</a>, & <a href="https://www.rethinkdb.com/" target="_BLANK">RethinkDB</a>.'
+    desc: 'Built a wedding RSVP manager using <a href="https://facebook.github.io/react/" target="_BLANK">React</a>, <a href="https://facebook.github.io/flux/docs/in-depth-overview.html#content" target="_BLANK">Flux</a>, & <a href="https://www.rethinkdb.com/" target="_BLANK">RethinkDB</a>. There is a form for entering/updating invites. RSVPs are individualized by URL and guests can easily search for a link via their email. After submitting the RSVP, guests receive an email confirmation and the database is updated.'
   },
 ];
