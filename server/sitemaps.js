7284ffe995ad340631fc7f0156dedc1888d58d90
lawshe <jessica@jlawshe.com>
2017-03-11 13:34:02 -0600

    Updated sitemap pages

--- server/sitemaps.js
+++ server/sitemaps.js
@@ -1,10 +1,6 @@
 sitemaps.add('/sitemap.xml', function() {
   return [
-    { page: '/', lastmod: '2015-05-25T12:00:00+00:00' },
-    { page: '/work', lastmod: '2015-05-25T12:00:00+00:00' },
-    { page: '/work/landes', lastmod: '2015-05-25T12:00:00+00:00' },
-    { page: '/work/bpb', lastmod: '2015-05-25T12:00:00+00:00' },
-    { page: '/work/bim_cubed', lastmod: '2015-05-25T12:00:00+00:00' },
-    { page: '/work/cherrywood', lastmod: '2015-05-29T12:00:00+00:00' }
+    { page: '/', lastmod: '2017-05-11T13:00:00+00:00' },
+    { page: '/work', lastmod: '2017-05-11T13:00:00+00:00' }
   ];
 });
