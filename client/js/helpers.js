425592bf2b70372da419e0895ca9fa294af2ff31
lawshe <jessica@jlawshe.com>
2017-03-11 12:20:10 -0600

    Template data helpers

---
+++ client/js/helpers.js
@@ -0,0 +1,27 @@
+if (Meteor.isClient) {
+	Template.about.helpers({
+		skills: function(){
+			return webSkills;
+		},
+		ie: function(){
+			return Session.get('ie');
+		},
+		examples: function(){
+			return Meteor.fxns.processExs(workExs);
+		}
+	});
+	Template.menu.helpers({
+		isAbout: function(){
+			if(Session.get('page') === 'about'){
+				return true;
+			}else{
+				return false;
+			}
+		}
+	});
+	Template.work.helpers({
+		examples: function(){
+			return Meteor.fxns.processExs(workExMore);
+		}
+	});
+}
