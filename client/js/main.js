dbaf5473fbc1fd8c2a5b03fa8a187ce4a5c161a8
lawshe <jessica@jlawshe.com>
2017-03-11 12:27:34 -0600

    Moved session variables to new file, removed unused template callbacks

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,3 @@
-Session.setDefault('section', '');
-Session.setDefault('page','');
-Session.setDefault('imgSize','sm');
-Session.setDefault('ie',false);
-Session.setDefault('pageDescription','');
-Session.setDefault('pageKeywords','');
 Meteor.startup(function() {
 	Meteor.fxns.msIE();
 	return SEO.config({
@@ -20,143 +14,53 @@ Meteor.startup(function() {
 		      og: true,
 		      set: ['description', 'url', 'title']
 		    }
-      });		
+      });
 });
-//if (Meteor.isClient) {
-	Template.layout.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			$('.container').show();
-		},
-		destroyed: function () {
-		}
-	});
-	Template.about.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			Session.set('page','about');
-			Meteor.fxns.allPages();
 
-			//svg
-			Meteor.about.drawMe();
-		},
-		destroyed: function () {
-			Meteor.fullpage.destroy();
-		}
-	});
-	Template.work.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			Session.set('page','work');
-			Meteor.fxns.allPages();
-		},
-		destroyed: function () {
-			Meteor.fullpage.destroy();
-		}
-	});
-	//landes
-	Template.landes.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			Session.set('page','landes');
-			Meteor.fxns.allPages();
-		},
-		destroyed: function () {
-			Meteor.fullpage.destroy();
-		}
-	});
-	//bpp
-	Template.bpb.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			Meteor.fxns.allPages();
-			Session.set('page','bpb');
-		},
-		destroyed: function () {
-			Meteor.fullpage.destroy();
-		}
-	});
-	//bim
-	Template.bim.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			Session.set('page','bim_cubed');
-			Meteor.fxns.allPages();
-		},
-		destroyed: function () {
-			Meteor.fullpage.destroy();
-		}
-	});
-	//cherrywood
-	Template.cherrywood.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			Session.set('page','cherrywood');
-			Meteor.fxns.allPages();
-		},
-		destroyed: function () {
-			Meteor.fullpage.destroy();
-		}
-	});
-	//not found
-	Template.notFound.hooks({
-		created: function () {
-		},
-		rendered: function () {
-			Meteor.fxns.allPages();
-		},
-		destroyed: function () {
-			Meteor.fullpage.destroy();
-		}
-	});
+/*
+	Layout
+*/
+Template.layout.hooks({
+	rendered: function () {
+		$('.container').show();
+	}
+});
 
-	//--EVENTS
-	Template.menu.events = {
-		'click .mobile-menu-toggle': function(){
-		    Meteor.fxns.toggleMobileMenu();
-		},
-		'click .menu-item.active': function(){
-			var menuId = $('.menu-item.active').attr('id');
-			if(menuId === 'menu-item-work'){
-				window.location ='/work';
-			}else{
-				location.reload();
-			}
-		},
-		'click .menu-item-sub': function(e){
-			e.preventDefault();
-			var anchor = $(e.currentTarget).attr('href').replace('#', '');
-			$.fn.fullpage.moveTo(anchor);
-		}
-	};
-	Template.fullpage.events = {
-		'click #fullpage': function(){
-			Meteor.fxns.checkMenu();
-		}
-	};
-	Template.about.events = {
-		'click .scroll-btn': function(e){
-			e.preventDefault();
-			Meteor.fxns.scrollBtn(e);
-		}
-	};
-	Template.work.events = {
-		'click .scroll-btn': function(e){
-			e.preventDefault();
-			Meteor.fxns.scrollBtn(e);
-		}
-	};
-	Template.clientPage.events = {
-		'click .scroll-btn': function(e){
-			e.preventDefault();
-			Meteor.fxns.scrollBtn(e);
-		}
-	};
-//}
+/*
+	About
+*/
+Template.about.hooks({
+	rendered: function () {
+		Session.set('page','about');
+		Meteor.fxns.allPages();
+		Meteor.about.drawMe();
+	},
+	destroyed: function () {
+		Meteor.fullpage.destroy();
+	}
+});
+
+/*
+	Work
+*/
+Template.work.hooks({
+	rendered: function () {
+		Session.set('page','work');
+		Meteor.fxns.allPages();
+	},
+	destroyed: function () {
+		Meteor.fullpage.destroy();
+	}
+});
+
+/*
+	404
+*/
+Template.notFound.hooks({
+	rendered: function () {
+		Meteor.fxns.allPages();
+	},
+	destroyed: function () {
+		Meteor.fullpage.destroy();
+	}
+});
