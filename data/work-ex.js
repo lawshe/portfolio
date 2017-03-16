09d41e6ddd7b6d282b0f909309b95ba5a203d1bf
lawshe <jessica@jlawshe.com>
2017-03-16 17:41:21 -0500

    Updated work text

--- data/work-ex.js
+++ data/work-ex.js
@@ -6,7 +6,7 @@ workExs = [
       file: 'fulltxt.gif',
       title: 'Full Text HTML from XML'
     },
-    desc: 'Article full text converted from <a href="https://jats.nlm.nih.gov/" target="_BLANK">JATS XML</a> to JSON, which is used in a template in <a href="https://www.meteor.com/" target="_BLANK">Meteor</a> to generate HTML. Article figures are served via a proxy &amp; hosted on <a href="https://aws.amazon.com/s3/" target="_BLANK">AWS S3</a>. The figures are optimized after uploading via <a href="https://aws.amazon.com/lambda/" target="_BLANK">AWS Lambda</a>.'
+    desc: 'For a peer reviewed bioscience research publisher, converted article full text <a href="https://jats.nlm.nih.gov/" target="_BLANK">JATS XML</a> to JSON, which is used in a <a href="https://www.meteor.com/" target="_BLANK">Meteor</a> template to generate HTML. Article figures are served via a proxy &amp; hosted on <a href="https://aws.amazon.com/s3/" target="_BLANK">AWS S3</a>. The figures are optimized after uploading via <a href="https://aws.amazon.com/lambda/" target="_BLANK">AWS Lambda</a>.'
   },
   {
     title: 'Payment Processing',
@@ -15,7 +15,7 @@ workExs = [
       file: '1.png',
       title: 'Checkout Step 1 using Authorize.net API'
     },
-    desc: 'A secure checkout with credit card processing. Payments are processed via <a href="https://www.authorize.net/" target="_BLANK">Authorize.net\'s</a> API. Upon completing, the user receives a confirmation email &amp; access to articles purchased.'
+    desc: 'A secure checkout with credit card processing. Payments are processed via <a href="https://www.authorize.net/" target="_BLANK">Authorize.net\'s</a> API. Upon completing, the user receives a confirmation email &amp; access to purchases.'
   },
   {
     title: 'Article Tweeting',
