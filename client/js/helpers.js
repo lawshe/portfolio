fdbff1b34d881b98fef2fd9a58559a4d0908dfb1
lawshe <jessica@jlawshe.com>
2017-03-27 14:39:14 -0500

    Changed template name (about -> home); Work data - process examples by year

--- client/js/helpers.js
+++ client/js/helpers.js
@@ -1,5 +1,5 @@
 if (Meteor.isClient) {
-	Template.about.helpers({
+	Template.home.helpers({
 		skills: function(){
 			return webSkills;
 		},
@@ -10,18 +10,14 @@ if (Meteor.isClient) {
 			return Meteor.fxns.processExs(workExs);
 		}
 	});
-	Template.menu.helpers({
-		isAbout: function(){
-			if(Session.get('page') === 'about'){
-				return true;
-			}else{
-				return false;
-			}
-		}
-	});
 	Template.work.helpers({
-		examples: function(){
-			return Meteor.fxns.processExs(workExs);
+		years: function(){
+			var years = workExMore.slice(0);
+			years.forEach(function(year){
+				year.id = year.year;
+				year.examples = Meteor.fxns.processExs(year.examples);
+			});
+			return years;
 		}
 	});
 }
