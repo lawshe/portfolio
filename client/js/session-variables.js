605d2c876406d331ec049384170359ca72459c70
lawshe <jessica@jlawshe.com>
2017-04-02 14:48:01 -0500

    Default imgSize null

--- client/js/session-variables.js
+++ client/js/session-variables.js
@@ -1,6 +1,6 @@
 Session.setDefault('section', '');
 Session.setDefault('page','');
-Session.setDefault('imgSize','sm');
+Session.setDefault('imgSize', null);
 Session.setDefault('ie',false);
 Session.setDefault('pageDescription','');
 Session.setDefault('pageKeywords','');
